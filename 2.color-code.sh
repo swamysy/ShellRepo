eb777d61644c25ec2de7a84c62d238802a5f4078
swamysy <swamy3sy@gmail.com>
2022-10-20 10:51:53 +0530

    Added print echo

---
+++ 2.color-code.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Color	    Foreground Code (Text)	Background Code
+# Black	        30	                    40
+# Red	        31	                    41
+# Green	        32	                    42
+# Yellow	    33	                    43
+# Blue	        34	                    44
+# Magenta	    35	                    45
+# Cyan	        36	                    46
+# White	        37	                    47
+# dark grey	    90	                    100
+# light red	    91	                    101
+# light green	92	                    102
+# light yellow	93	                    103
+# light blue	94	                    104
+# light purple	95	                    105
+# turquoise	    96	                    106
+# light white	97	                    107
+
+# echo -e "\e[1;31m This is red text \e[0m"
+
+echo -e "\e[1;31m This is red text \e[0m"
