f7dac9c854d8e12f38f3a41612ddc64f3d146940
swamysy <swamy3sy@gmail.com>
2022-10-25 22:25:29 +0530

    components te

--- Roboshop/components/frontend.sh
+++ Roboshop/components/frontend.sh
@@ -3,6 +3,7 @@ set -e
 
 # Verify the scripts been executed using root user or not.
 USERID=$(id -u)
+COMPONENT=frontend
 
 if [ $USERID -ne 0 ] ; then
     echo -e "\e[31m You must run this script as a root user or with sudo privilige \e[0m"
@@ -18,7 +19,7 @@ stat() {
 }
 
 echo -n "Installing nginx package"
-yum install nginx -y &>> /tmp/frontend.log
+yum install nginx -y &>> /tmp/$COMPONENT.log
 stat $?
 
 echo -n "Downloading the frontend component"
@@ -26,15 +27,22 @@ curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend
 stat $?
 
 echo -n "Performing Cleanup"
-rm -rf /usr/share/nginx/html/* &>> /tmp/frontend.log
+rm -rf /usr/share/nginx/html/* &>> /tmp/$COMPONENT.log
 stat $?
 
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip &>> /tmp/frontend.log
+
+echo -n "Unzipping the component"
+unzip /tmp/frontend.zip &>> /tmp/$COMPONENT.log
+stat $?
+
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
+
+echo -n "Configuring the reverse proxy settings"
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
+stat $?
 
 # cd /usr/share/nginx/html
 # rm -rf *
@@ -44,5 +52,7 @@ mv localhost.conf /etc/nginx/default.d/roboshop.conf
 # rm -rf frontend-main README.md
 # mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
-systemctl enable nginx &>> /tmp/frontend.log
-systemctl start nginx &>> /tmp/frontend.log
+echo -n "Starting the nginx service"
+systemctl enable nginx &>> /tmp/$COMPONENT.log
+systemctl start nginx &>> /tmp/$COMPONENT.log
+stat $?
