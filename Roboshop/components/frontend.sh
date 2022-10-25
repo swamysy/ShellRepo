4d513a26cdd3d1dbe52a8fffcb62bd533db93505
swamysy <swamy3sy@gmail.com>
2022-10-25 21:46:04 +0530

    components tes

--- Roboshop/components/frontend.sh
+++ Roboshop/components/frontend.sh
@@ -9,7 +9,15 @@ if [ $USERID -ne 0 ] ; then
     exit 1
 fi
 
-yum install nginx -y
+echo "Installing nginx package"
+yum install nginx -y &>> /tmp/frontend.log
+if [ $? -le 0 ] ; then
+    echo -e "\e[32m Success \e[0m"
+else 
+    echo -e "\e[31m Failure \e[0m"
+fi
+
+echo "Downloading the frontend component"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
 rm -rf /usr/share/nginx/html/*
