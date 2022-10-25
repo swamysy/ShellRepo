007f9e9040efd66976545795911b762b9a95193f
swamysy <swamy3sy@gmail.com>
2022-10-25 21:40:27 +0530

    components

--- Roboshop/components/frontend.sh
+++ Roboshop/components/frontend.sh
@@ -4,7 +4,7 @@ set -e
 # Verify the scripts been executed using root user or not.
 UID=$(id -u)
 
-if [ $UID -ne 0 ] ; then
+if [$UID -ne 0] ; then
     echo -e "\e[31m You must run this script as a root user or with sudo privilige \e[0m"
     exit 1
 fi
