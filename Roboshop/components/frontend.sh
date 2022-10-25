e9404fe36b68a56d02ecc0a46160ea979ddd379d
swamysy <swamy3sy@gmail.com>
2022-10-25 21:50:40 +0530

    components tes

--- Roboshop/components/frontend.sh
+++ Roboshop/components/frontend.sh
@@ -9,7 +9,7 @@ if [ $USERID -ne 0 ] ; then
     exit 1
 fi
 
-echo "Installing nginx package"
+echo -n "Installing nginx package"
 yum install nginx -y &>> /tmp/frontend.log
 if [ $? -le 0 ] ; then
     echo -e "\e[32m Success \e[0m"
@@ -17,12 +17,24 @@ else
     echo -e "\e[31m Failure \e[0m"
 fi
 
-echo "Downloading the frontend component"
+echo -n "Downloading the frontend component"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+if [ $? -le 0 ] ; then
+    echo -e "\e[32m Success \e[0m"
+else 
+    echo -e "\e[31m Failure \e[0m"
+fi
 
-rm -rf /usr/share/nginx/html/*
+echo -n "Performing Cleanup"
+rm -rf /usr/share/nginx/html/* &>> /tmp/frontend.log
+if [ $? -le 0 ] ; then
+    echo -e "\e[32m Success \e[0m"
+else 
+    echo -e "\e[31m Failure \e[0m"
+fi
+
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>> /tmp/frontend.log
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
@@ -36,5 +48,5 @@ mv localhost.conf /etc/nginx/default.d/roboshop.conf
 # rm -rf frontend-main README.md
 # mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
-systemctl enable nginx
-systemctl start nginx
+systemctl enable nginx &>> /tmp/frontend.log
+systemctl start nginx &>> /tmp/frontend.log
