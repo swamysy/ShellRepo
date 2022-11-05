d81f6d6b5967f2c68c1680a5c84ab7119c0ade87
swamysy <swamy3sy@gmail.com>
2022-11-05 11:29:14 +0530

    Testing mongodb

--- Roboshop/components/mongodb.sh
+++ Roboshop/components/mongodb.sh
@@ -15,3 +15,25 @@ stat $?
 echo -n "Updating the mongodb config"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 stat $?
+
+echo -n "Starting MongoDb Service"
+systemctl enable mongod &>> $LOGFILE
+systemctl start mongod &>> $LOGFILE
+
+echo -n "Downloading the $COMPONENT Schema"
+curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
+stat $?
+
+echo -n "Injecting the $COMPONENT Schema"
+cd /tmp
+unzip mongodb.zip
+cd mongodb-main
+mongo < catalogue.js &>> $LOGFILE
+mongo < users.js &>> $LOGFILE
+stat $?
+
+# cd /tmp
+# unzip mongodb.zip
+# cd mongodb-main
+# mongo < catalogue.js
+# mongo < users.js
