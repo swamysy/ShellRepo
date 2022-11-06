9632c49914edb3410318cdf11de5f3356283e97a
swamysy <swamy3sy@gmail.com>
2022-11-06 15:38:23 +0530

    Testing mongodb

--- Roboshop/components/rabbitmq.sh
+++ Roboshop/components/rabbitmq.sh
@@ -2,9 +2,28 @@
 set -e
 
 COMPONENT=rabbitmq
-APPUSER=roboshop
 source components/common.sh
 
-NODEJS
+echo -n "Installing and configuring $COMPONENT repo"
+yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y &>> $LOGFILE
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>> $LOGFILE
+stat $?
 
-echo -n "_______________$COMPONENT Installation is Successful______________"
+echo -n "Installing $COMPONENT : "
+yum install rabbitmq-server -y &>> $LOGFILE
+stat $?
+
+echo -n "Starting $COMPONENT : "
+systemctl enable rabbitmq-server &>> $LOGFILE
+systemctl start rabbitmq-server &>> $LOGFILE
+stat $?
+
+echo -n "Creating Application user on $COMPONENT: "
+rabbitmqctl add_user roboshop roboshop123
+stat $?
+
+# rabbitmqctl add_user roboshop roboshop123
+# rabbitmqctl set_user_tags roboshop administrator
+# rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+
+echo -e "\e[32m_______________$COMPONENT Installation is Successful______________\e[0m"
