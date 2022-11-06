6512f2cfc710197cc5b4ca79e2123aaee9057736
swamysy <swamy3sy@gmail.com>
2022-11-06 15:40:31 +0530

    Testing mongodb

--- Roboshop/components/rabbitmq.sh
+++ Roboshop/components/rabbitmq.sh
@@ -5,7 +5,7 @@ COMPONENT=rabbitmq
 source components/common.sh
 
 echo -n "Installing and configuring $COMPONENT repo"
-yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y &>> $LOGFILE
+#yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y &>> $LOGFILE
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>> $LOGFILE
 stat $?
 
