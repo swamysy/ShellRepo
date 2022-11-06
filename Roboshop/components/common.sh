9c39ea99697aa209caf3aa0e6a3c3320e8850080
swamysy <swamy3sy@gmail.com>
2022-11-06 21:40:19 +0530

    Testing mongodb

--- Roboshop/components/common.sh
+++ Roboshop/components/common.sh
@@ -68,7 +68,7 @@ PYTHON() {
     GROUPID=${id -g roboshop}
 
     echo -n "Updating the uid and gid with $APPUSER in $PAYMENT.ini : "
-    sed -i -e "/^uid/ c uid=$USERID" -e "/^gid/ c gid=$GROUPID" $COMPONENT.ini
+    sed -i -e "/^uid/ c uid=$USERID" -e "/^gid/ c gid=$GROUPID" /home/$APPUSER/$COMPONENT.ini
     stat $?
 
     # CONFIGURING SERVICES
