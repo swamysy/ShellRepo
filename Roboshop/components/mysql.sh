3bed24de66a23ad5dc54930a9810ccf00f9cc8e9
swamysy <swamy3sy@gmail.com>
2022-11-06 13:25:41 +0530

    New Changes

--- Roboshop/components/mysql.sh
+++ Roboshop/components/mysql.sh
@@ -17,5 +17,22 @@ echo -n "Starting $COMPONENT service: "
 systemctl enable mysqld  && systemctl start mysqld
 stat $?
 
-# echo -n "Changing the default password: "
-# grep 'temperarory password' /var/log/mysqld.log
+echo -n "Changing the default password: "
+DEF_ROOT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk -F ' ' '{print $NF}')
+
+# I only want to change the default password only for the first time.
+# How do I come to know whether the password is changed or not.
+
+echo show databases | mysql -uroot -pRoboShop@1 &>> $LOGFILE
+if [ $? -ne 0 ] ; then
+    echo -n "Reset Root Password"
+    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" | mysql --connect-expired-password -uroot -p"${DEF_ROOT_PASSWORD}" &>> $LOGFILE
+    stat $?
+fi
+
+echo show plugins | mysql -uroot -pRoboShop@1 | grep validate_password; &>> $LOGFILE
+if [ $? -ne 0 ] ; then
+    echo -n "Uninstalling the password validate plugin: "
+    echo "uninstall plugin validate_password;" | mysql --connect-expired-password -uroot -pRoboshop@1 &>> $LOGFILE
+    stat $?
+fi
