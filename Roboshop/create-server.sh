3a5e5bdb821ee2babfb5581e1d661dc3e4ecc08c
swamysy <swamy3sy@gmail.com>
2022-11-08 00:26:40 +0530

    New Changes

--- Roboshop/create-server.sh
+++ Roboshop/create-server.sh
@@ -18,3 +18,6 @@ echo $COMPONENT
 PRIVATE_IP="$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SG_ID --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')"
 
 sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/$COMPONENT/" route53.json > /tmp/dns.json
+
+echo -n "Creating the DNS Record *********"
+aws route53 change-resource-record-sets --hosted-zone-id Z006364810T8FNJLH82AQ --change-batch file:///tmp/dns.json | jq
