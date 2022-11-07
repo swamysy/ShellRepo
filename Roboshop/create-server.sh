53edc73bc52878f5261d138a4c0084f1a319345c
swamysy <swamy3sy@gmail.com>
2022-11-07 23:49:20 +0530

    Testing mongodb

--- Roboshop/create-server.sh
+++ Roboshop/create-server.sh
@@ -6,4 +6,6 @@ echo "AMI ID Used to launch instance is : $AMI_ID"
 echo "SG ID Used to launch instance is : $SG_ID"
 COMPONENT=$1
 echo $COMPONENT
-aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
+#aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
+
+aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SG_ID  --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehaviour=stop}" --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
