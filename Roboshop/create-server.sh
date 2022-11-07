5fe0ebce77267a23728d849e23ab32052e1f8fa1
swamysy <swamy3sy@gmail.com>
2022-11-07 23:22:47 +0530

    New Changes

--- Roboshop/create-server.sh
+++ Roboshop/create-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+$COMPONENT=$1
 AMI_ID="$(aws ec2 describe-images --region us-east-1 --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')"
 SG_ID="$(aws ec2 describe-security-groups --filters Name=group-name,Values=b51-allow-all | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')"
 echo "AMI ID Used to launch instance is : $AMI_ID"
 echo "SG ID Used to launch instance is : $SG_ID"
-aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro | jq
+aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq
